e975f32a84c30892e198a2b5a69b52eeb7b780ac
Brian Shewan <brian.shewan@nscc.ca>
2022-11-30 15:18:14 -0400

    added more crappy database examples

--- crappy-db.psm1
+++ crappy-db.psm1
@@ -4,6 +4,29 @@ function Initialize-CrappyDB {
         New-Item -Path 'data' -ItemType Directory > $null
     }
     Set-Location 'data'
+    New-Item 'crappydb.txt'
 }
 
-Export-ModuleMember -Function Initialize-CrappyDB
+function Add-Data {
+    $records = @(
+        @("w111111","john","smith"),
+        @("w222222","jane","doe"),
+        @("w333333","jack","sprat")
+    )
+
+    Set-Content -Path "crappydb.txt" -Value "" -NoNewline
+    foreach ($record in $records) {
+        Add-Content -Path "crappydb.txt" -Value ($record -join ',')
+    }
+}
+
+function Select-Data {
+    $data = Get-Content -Path "crappydb.txt"
+    foreach ($line in $data) {
+        if ($line -like "*jack*") {
+            Write-Host $line
+        }
+    }
+}
+
+Export-ModuleMember -Function Initialize-CrappyDB, Add-Data, Select-Data
